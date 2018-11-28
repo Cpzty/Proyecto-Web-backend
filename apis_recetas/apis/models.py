670d6e9a98ebce10d5815cfc5e9f643cc87eaf57
Yasmin Chavez <ychavez2014ig@gmail.com>
2018-11-28 13:37:17 -0600

    permissions

--- apis_recetas/apis/models.py
+++ apis_recetas/apis/models.py
@@ -2,38 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-#Permisos
-"""
-class RecetaPermissions(BasePermission):
-    def has_permission(self, request, view):
-        if view.action == 'create':
-            return user.has_permission('canoneras.add_canonera')
-        elif view.action == 'list':
-            return True
-        return False
-
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        if view.action == 'retrieve':
-            return user.has_permission('canoneras.view_canonera') or user == obj.created_by
-        elif view.action == 'destroy':
-            return user.has_permission('canoneras.remove_canonera')
-        elif view.action == 'partial_update':
-            return user.has_permission('canoneras.change_canonera', obj) or user.has_permission('canoneras.change_canonera')
-        elif view.action == 'send_reminder':
-            return True
-        return False
-
-"""
-
-
 # Create your models here.
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50,blank=True)
 
 
 class Receta(models.Model):
-    nombre = models.CharField(max_length=50,blank=True)
+    nombre = models.CharField(max_length=50,blank=True, null=False)
     ingrediente = models.TextField()
     preparacion = models.TextField()
     categoria_id= models.ForeignKey(Categoria, models.SET_NULL, blank=True, null=True)
--- apis_recetas/apis/views.py
+++ apis_recetas/apis/views.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
+from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import generics
 
 from . import models, serializers
-    
+
+#Permisos
+class RecetasPermissions(BasePermission):
+    def has_permission(self, request, view):
+        if view.action == 'create':
+            return user.has_permission('apis.add_receta')
+        elif view.action == 'list':
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if view.action == 'retrieve':
+            return user.has_permission('apis.view_receta') or user == obj.created_by
+        elif view.action == 'destroy':
+            return user.has_permission('apis.remove_receta')
+        elif view.action == 'partial_update':
+            return user.has_permission('apis.change_receta', obj) or user.has_permission('apis.change_receta')
+        elif view.action == 'send_reminder':
+            return True
+        return False
+
+      
 class CategoriaModelViewSet(viewsets.ModelViewSet):
     queryset = models.Categoria.objects.all()
     serializer_class = serializers.CategoriaSerializer
@@ -14,5 +37,5 @@ class CategoriaModelViewSet(viewsets.ModelViewSet):
 class RecetaModelViewSet(viewsets.ModelViewSet):
     queryset = models.Receta.objects.all()
     serializer_class = serializers.RecetaSerializer
-    permission_classes = [permissions.AllowAny, ]
+    permission_classes = [RecetasPermissions]
 
